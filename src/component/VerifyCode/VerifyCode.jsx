c1c60338ad0889b00e848ecfd63909eaaf27cb71
eslamabosalem <eslambosalem22@gmail.com>
2024-08-29 09:01:25 +0300

    done

--- src/component/VerifyCode/VerifyCode.jsx
+++ src/component/VerifyCode/VerifyCode.jsx
@@ -32,20 +32,21 @@ const VerifyCode = () => {
 
     return (
         <div style={{ padding: '20px', maxWidth: '400px', margin: 'auto' ,marginTop:"100px" }}>
-            <h2>Verify Reset Code</h2>
+            <h2 className='text-2xl text-green-500'>Verify Reset Code</h2>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="resetCode">Reset Code:</label>
+                <label htmlFor="resetCode " className='text-xl text-green-500'>Reset Code:</label>
                 <input
                     type="text"
                     id="resetCode"
                     value={resetCode}
                     onChange={(e) => setResetCode(e.target.value)}
                     required
-                    style={{ display: 'block', width: '100%', marginBottom: '10px' }}
+                    className='block py-2.5 px-0 w-full text-sm text-green-900 bg-transparent border-0 border-b-2 border-green-300 appearance-none dark:text-white dark:border-green-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer'
+
                 />
-                <button type="submit" style={{ display: 'block', width: '100%' }}>
-                    Verify Code
-                </button>
+                    <button type="submit" className="text-white bg-green-700 my-5 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">  Verify Code</button>
+                   
+               
             </form>
             <p>{message}</p>
         </div>
